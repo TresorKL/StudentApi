6d3df61156588c67855700cc6cdd0f96b62ef087
TresorKL <trezmathieu@gmail.com>
2022-10-28 12:17:29 +0200

    fetch models from mysql db

--- studentapi/settings.py
+++ studentapi/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -45,7 +49,7 @@ INSTALLED_APPS = [
 CORS_ORIGIN_ALLOW_ALL =True
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddlewore'
+    # 'corsheaders.middleware.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -83,6 +87,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'StudentProject',
+        'USER':'root',
         'PASSWORD':'12345678',
         'PORT':3306,
         'HOST':'localhost'
